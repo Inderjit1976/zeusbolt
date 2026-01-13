17985af1f3656c54f75a5d9412b3ea2b47eb87c9
Inderjit1976 <imann@live.co.uk>
2026-01-13 11:43:43 +0000

    Add homepage route

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>ZeusBolt</h1>
+      <p>
+        Welcome to ZeusBolt. This is the homepage.
+      </p>
+
+      <p>
+        <a href="/dashboard">Go to Dashboard</a>
+      </p>
+    </div>
+  );
+}
